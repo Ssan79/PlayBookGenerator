9826c2bfcae3263469b37620a61191b1b5a31153
Saravanan Lakshmanan <Ssan79@users.noreply.github.com>
2025-02-24 15:11:13 +0800

    Play Features added

--- app.py
+++ app.py
@@ -1,56 +1,32 @@
-from flask import Flask, render_template, request, jsonify
-import yaml
+from flask import Flask, request, Response, send_from_directory
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Serve the HTML form (index.html) at the root URL
+@app.route("/", methods=["GET"])
+def serve_index():
+    # If you have 'index.html' in the same directory:
+    return send_from_directory('.', 'index.html')
 
 
-@app.route('/generate', methods=['POST'])
-def generate_playbook():
-    tasks = []
+@app.route("/generate", methods=["POST"])
+def generate():
+    # 'playbook' is the hidden input (or client-side field) containing the final YAML
+    playbook_yaml = request.form.get("playbook", "")
 
-    num_tasks = len(request.form.getlist('tasks'))
+    if not playbook_yaml.strip():
+        return "No playbook data was supplied."
 
-    for i in range(num_tasks):
-        task = {}
-        task_name = request.form.getlist('tasks')[i]
-        task_details = request.form.getlist('actions')[i]
-        hosts = request.form.getlist('hosts')[i]
-        become_task = request.form.getlist('become_task')[i]
-        condition = request.form.getlist('conditions')[i]
-        condition_type = request.form.getlist('conditions_type')[i]
-        condition_value = request.form.getlist('conditions_value')[i]
-        message = request.form.getlist('messages')[i]
-        tags = request.form.getlist('tags')[i]
-        tags_name = request.form.getlist('tags_name')[i]
-
-        task['hosts'] = hosts
-        if become_task == 'yes':
-            task['become'] = True
-        task['tasks'] = [{'name': task_name}]
-
-        task_details_list = task_details.split('\n')
-        task['tasks'][0].update({'action': task_details_list})
-
-        if condition == 'yes' and condition_type and condition_value:
-            task['tasks'][0][condition_type] = condition_value
-
-        if tags == 'yes' and tags_name:
-            task['tasks'][0]['tags'] = tags_name
-
-        if message:
-            task['tasks'].append({'debug': {'msg': message}})
-
-        tasks.append(task)
-
-    playbook_yaml = yaml.dump(tasks, sort_keys=False, default_flow_style=False)
-
-    return jsonify({'playbook': playbook_yaml})
+    # Return the YAML so the browser downloads it as "playbook.yml"
+    return Response(
+        playbook_yaml,
+        mimetype="text/yaml",
+        headers={"Content-Disposition": "attachment; filename=playbook.yml"}
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # Run Flask app on http://localhost:5000/
     app.run(debug=True)
